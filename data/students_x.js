ef728c64960ac06ee0b7c3326b32255fd3811290
Yiannis Koutis <46483013+ikoutis@users.noreply.github.com>
2024-10-18 22:36:43 -0400

    Create students_x.js

---
+++ data/students_x.js
@@ -0,0 +1,7 @@
+// students.js
+export const students = [
+    { name: "A M Muntasir Rahman", email: "ar238@njit.edu", webpage: "https://sites.google.com/njit.edu/ammuntasirrahman/", linkedin: "https://www.linkedin.com/in/a-m-muntasir-rahman/", scholar: "https://scholar.google.com/citations?user=_inhYXAAAAAJ&hl=en" },
+    { name: "Ahmad Alemari", email: "aaa376@njit.edu", linkedin: "https://www.linkedin.com/in/ahmad-alemari-927661121/" },
+    { name: "Akhitha S. Manjitha", email: "asm277@njit.edu", webpage: "https://asmanjitha.github.io/Akhitha_Manjitha/", linkedin: "https://www.linkedin.com/in/asmanjitha", github: "https://github.com/asmanjitha" },
+    // Add more students here...
+];
