150aafbef06377552efc5cb819c1f03de555cb1f
Yiannis Koutis <46483013+ikoutis@users.noreply.github.com>
2024-11-17 00:48:44 -0500

    Update cs-faculty.js

--- data/cs-faculty.js
+++ data/cs-faculty.js
@@ -1,294 +1,292 @@
 export const facultyData = [
   {
-    ""name"": ""David Bader"",
-    ""webpage"": ""https://davidbader.net/"",
-    ""areas"": [
-      ""Artificial intelligence"",
-      ""High-performance computing"",
-      ""Algorithms & complexity"",
-      ""Computational biology & bioinformatics"",
-      ""Comp. Science and Engineering""
+    "name": "David Bader",
+    "webpage": "https://davidbader.net/",
+    "areas": [
+      "Artificial intelligence",
+      "High-performance computing",
+      "Algorithms & complexity",
+      "Computational biology & bioinformatics",
+      "Comp. Science and Engineering"
     ]
   },
   {
-    ""name"": ""Cristian Borcea"",
-    ""webpage"": ""https://web.njit.edu/~borcea/"",
-    ""areas"": [
-      ""Machine learning"",
-      ""The Web & information retrieval"",
-      ""Computer security"",
-      ""Mobile computing""
+    "name": "Cristian Borcea",
+    "webpage": "https://web.njit.edu/~borcea/",
+    "areas": [
+      "Machine learning",
+      "The Web & information retrieval",
+      "Computer security",
+      "Mobile computing"
     ]
   },
   {
-    ""name"": ""Mark Cartwright"",
-    ""webpage"": ""https://markcartwright.com/"",
-    ""areas"": [
-      ""Artificial intelligence"",
-      ""Machine learning"",
-      ""Human-computer interaction"",
-      ""Sound and music computing""
+    "name": "Mark Cartwright",
+    "webpage": "https://markcartwright.com/",
+    "areas": [
+      "Artificial intelligence",
+      "Machine learning",
+      "Human-computer interaction",
+      "Sound and music computing"
     ]
   },
   {
-    ""name"": ""Reza Curtmola"",
-    ""webpage"": ""https://web.njit.edu/~crix/"",
-    ""areas"": [
-      ""Computer security"",
-      ""Cryptography"",
-      ""Privacy Enhancing Technologies""
+    "name": "Reza Curtmola",
+    "webpage": "https://web.njit.edu/~crix/",
+    "areas": [
+      "Computer security",
+      "Cryptography",
+      "Privacy Enhancing Technologies"
     ]
   },
   {
-    ""name"": ""Salam Daher"",
-    ""webpage"": ""https://vitalhsnjit.com/"",
-    ""areas"": [
-      ""Computer graphics"",
-      ""Computer science education"",
-      ""Human-computer interaction"",
-      ""Medical Informatics"",
-      ""Visualization"",
-      ""Healthcare Simulation""
+    "name": "Salam Daher",
+    "webpage": "https://vitalhsnjit.com/",
+    "areas": [
+      "Computer graphics",
+      "Computer science education",
+      "Human-computer interaction",
+      "Medical Informatics",
+      "Visualization",
+      "Healthcare Simulation"
     ]
   },
   {
-    ""name"": ""Xiaoning Ding"",
-    ""webpage"": ""https://web.njit.edu/~dingxn"",
-    ""areas"": [
-      ""Computer architecture"",
-      ""High-performance computing"",
-      ""Operating systems"",
-      ""Cloud Computing""
+    "name": "Xiaoning Ding",
+    "webpage": "https://web.njit.edu/~dingxn",
+    "areas": [
+      "Computer architecture",
+      "High-performance computing",
+      "Operating systems",
+      "Cloud Computing"
     ]
   },
   {
-    ""name"": ""James Geller"",
-    ""webpage"": ""https://web.njit.edu/~geller/"",
-    ""areas"": [
-      ""Artificial intelligence"",
-      ""Medical Informatics"",
-      ""Knowledge Graphs"",
-      ""Knowledge Graphs""
+    "name": "James Geller",
+    "webpage": "https://web.njit.edu/~geller/",
+    "areas": [
+      "Artificial intelligence",
+      "Medical Informatics",
+      "Knowledge Graphs"
     ]
   },
   {
-    ""name"": ""Alex Gerbessiotis"",
-    ""webpage"": ""https://web.njit.edu/~alexg/"",
-    ""areas"": [
-      ""High-performance computing"",
-      ""Algorithms & complexity"",
-      ""Economics & computation""
+    "name": "Alex Gerbessiotis",
+    "webpage": "https://web.njit.edu/~alexg/",
+    "areas": [
+      "High-performance computing",
+      "Algorithms & complexity",
+      "Economics & computation"
     ]
   },
   {
-    ""name"": ""Kasthuri Jayarajah"",
-    ""webpage"": ""https://kjayarajah.github.io"",
-    ""areas"": [
-      ""Artificial intelligence"",
-      ""Mobile computing"",
-      ""Human-computer interaction"",
-      ""Robotics""
+    "name": "Kasthuri Jayarajah",
+    "webpage": "https://kjayarajah.github.io",
+    "areas": [
+      "Artificial intelligence",
+      "Mobile computing",
+      "Human-computer interaction",
+      "Robotics"
     ]
   },
   {
-    ""name"": ""Amy K. Hoover"",
-    ""webpage"": ""https://amykhoover.com"",
-    ""areas"": [
-      ""Artificial intelligence"",
-      ""Machine learning"",
-      ""Evolutionary Computing""
+    "name": "Amy K. Hoover",
+    "webpage": "https://amykhoover.com",
+    "areas": [
+      "Artificial intelligence",
+      "Machine learning",
+      "Evolutionary Computing"
     ]
   },
   {
-    ""name"": ""Martin Kellogg"",
-    ""webpage"": ""https://kelloggm.github.io/martinjkellogg.com/"",
-    ""areas"": [
-      ""Programming languages"",
-      ""Software engineering"",
-      ""Logic & verification""
+    "name": "Martin Kellogg",
+    "webpage": "https://kelloggm.github.io/martinjkellogg.com/",
+    "areas": [
+      "Programming languages",
+      "Software engineering",
+      "Logic & verification"
     ]
   },
   {
-    ""name"": ""Ioannis Koutis"",
-    ""webpage"": ""https://web.njit.edu/~ikoutis/"",
-    ""areas"": [
-      ""Artificial intelligence"",
-      ""Machine learning"",
-      ""Design automation"",
-      ""Algorithms & complexity""
+    "name": "Ioannis Koutis",
+    "webpage": "https://web.njit.edu/~ikoutis/",
+    "areas": [
+      "Artificial intelligence",
+      "Machine learning",
+      "Design automation",
+      "Algorithms & complexity"
     ]
   },
   {
-    ""name"": ""Jing Li"",
-    ""webpage"": ""https://scholar.google.com/citations?user=eFjZ6jAAAAAJ&hl=en"",
-    ""areas"": [
-      ""Artificial intelligence"",
-      ""Design automation"",
-      ""Embedded & real-time systems"",
-      ""High-performance computing"",
-      ""Algorithms & complexity""
+    "name": "Jing Li",
+    "webpage": "https://scholar.google.com/citations?user=eFjZ6jAAAAAJ&hl=en",
+    "areas": [
+      "Artificial intelligence",
+      "Design automation",
+      "Embedded & real-time systems",
+      "High-performance computing",
+      "Algorithms & complexity"
     ]
   },
   {
-    ""name"": ""Chengjun Liu"",
-    ""webpage"": ""https://web.njit.edu/~cliu"",
-    ""areas"": [
-      ""Artificial intelligence"",
-      ""Computer vision"",
-      ""Machine learning""
+    "name": "Chengjun Liu",
+    "webpage": "https://web.njit.edu/~cliu",
+    "areas": [
+      "Artificial intelligence",
+      "Computer vision",
+      "Machine learning"
     ]
   },
   {
-    ""name"": ""Nathan Malkin"",
-    ""webpage"": ""https://malk.in"",
-    ""areas"": [
-      ""Computer security"",
-      ""Human-computer interaction"",
-      ""Privacy Enhancing Technologies""
+    "name": "Nathan Malkin",
+    "webpage": "https://malk.in",
+    "areas": [
+      "Computer security",
+      "Human-computer interaction",
+      "Privacy Enhancing Technologies"
     ]
   },
   {
-    ""name"": ""Marvin Nakayama"",
-    ""webpage"": ""https://web.njit.edu/~marvin/"",
-    ""areas"": [
-      ""Measurement & perf. analysis"",
-      ""Algorithms & complexity""
+    "name": "Marvin Nakayama",
+    "webpage": "https://web.njit.edu/~marvin/",
+    "areas": [
+      "Measurement & perf. analysis",
+      "Algorithms & complexity"
     ]
   },
   {
-    ""name"": ""Iulian Neamtiu"",
-    ""webpage"": ""https://web.njit.edu/~ineamtiu/"",
-    ""areas"": [
-      ""Machine learning"",
-      ""Computer security"",
-      ""Mobile computing"",
-      ""Programming languages"",
-      ""Software engineering""
+    "name": "Iulian Neamtiu",
+    "webpage": "https://web.njit.edu/~ineamtiu/",
+    "areas": [
+      "Machine learning",
+      "Computer security",
+      "Mobile computing",
+      "Programming languages",
+      "Software engineering"
     ]
   },
   {
-    ""name"": ""Baruch Schieber"",
-    ""webpage"": ""https://sbarnjit.github.io/"",
-    ""areas"": [
-      ""The Web & information retrieval"",
-      ""Databases"",
-      ""Algorithms & complexity"",
-      ""Computational biology & bioinformatics""
+    "name": "Baruch Schieber",
+    "webpage": "https://sbarnjit.github.io/",
+    "areas": [
+      "The Web & information retrieval",
+      "Databases",
+      "Algorithms & complexity",
+      "Computational biology & bioinformatics"
     ]
   },
   {
-    ""name"": ""Shantanu Sharma"",
-    ""webpage"": ""https://web.njit.edu/~ss797/"",
-    ""areas"": [
-      ""Computer security"",
-      ""Databases"",
-      ""Cryptography""
+    "name": "Shantanu Sharma",
+    "webpage": "https://web.njit.edu/~ss797/",
+    "areas": [
+      "Computer security",
+      "Databases",
+      "Cryptography"
     ]
   },
   {
-    ""name"": ""Cong Shi"",
-    ""webpage"": ""https://web.njit.edu/~cs638/"",
-    ""areas"": [
-      ""Machine learning"",
-      ""Computer security"",
-      ""Mobile computing""
+    "name": "Cong Shi",
+    "webpage": "https://web.njit.edu/~cs638/",
+    "areas": [
+      "Machine learning",
+      "Computer security",
+      "Mobile computing"
     ]
   },
   {
-    ""name"": ""Frank Shih"",
-    ""webpage"": ""https://web.njit.edu/~shih/"",
-    ""areas"": [
-      ""Artificial intelligence"",
-      ""Computer vision"",
-      ""Machine learning"",
-      ""Natural language processing"",
-      ""Visualization""
+    "name": "Frank Shih",
+    "webpage": "https://web.njit.edu/~shih/",
+    "areas": [
+      "Artificial intelligence",
+      "Computer vision",
+      "Machine learning",
+      "Natural language processing",
+      "Visualization"
     ]
   },
   {
-    ""name"": ""Margarita Vinnikov"",
-    ""webpage"": ""https://www.margaritavinnikov.com/"",
-    ""areas"": [
-      ""Computer graphics"",
-      ""Computer science education"",
-      ""Human-computer interaction"",
-      ""Visualization"",
-      ""Extended Reality""
+    "name": "Margarita Vinnikov",
+    "webpage": "https://www.margaritavinnikov.com/",
+    "areas": [
+      "Computer graphics",
+      "Computer science education",
+      "Human-computer interaction",
+      "Visualization",
+      "Extended Reality"
     ]
   },
   {
-    ""name"": ""Jason Wang"",
-    ""webpage"": ""https://web.njit.edu/~wangj/"",
-    ""areas"": [
-      ""Artificial intelligence"",
-      ""Computer vision"",
-      ""Machine learning"",
-      ""Databases"",
-      ""High-performance computing"",
-      ""Solar Physics and Space Weather"",
-      ""Solar Physics and Space Weather""
+    "name": "Jason Wang",
+    "webpage": "https://web.njit.edu/~wangj/",
+    "areas": [
+      "Artificial intelligence",
+      "Computer vision",
+      "Machine learning",
+      "Databases",
+      "High-performance computing",
+      "Solar Physics and Space Weather"
     ]
   },
   {
-    ""name"": ""Lijing Wang"",
-    ""webpage"": ""https://christa60.github.io/"",
-    ""areas"": [
-      ""Artificial intelligence"",
-      ""Machine learning"",
-      ""Natural language processing"",
-      ""Computational biology & bioinformatics"",
-      ""Medical Informatics""
+    "name": "Lijing Wang",
+    "webpage": "https://christa60.github.io/",
+    "areas": [
+      "Artificial intelligence",
+      "Machine learning",
+      "Natural language processing",
+      "Computational biology & bioinformatics",
+      "Medical Informatics"
     ]
   },
   {
-    ""name"": ""Tomer Weiss"",
-    ""webpage"": ""https://tomerwei.github.io/"",
-    ""areas"": [
-      ""Artificial intelligence"",
-      ""Computer vision"",
-      ""Computer graphics"",
-      ""Visualization""
+    "name": "Tomer Weiss",
+    "webpage": "https://tomerwei.github.io/",
+    "areas": [
+      "Artificial intelligence",
+      "Computer vision",
+      "Computer graphics",
+      "Visualization"
     ]
   },
   {
-    ""name"": ""Chase Wu"",
-    ""webpage"": ""https://web.njit.edu/~chasewu/"",
-    ""areas"": [
-      ""Machine learning"",
-      ""Computer networks"",
-      ""High-performance computing"",
-      ""Algorithms & complexity""
+    "name": "Chase Wu",
+    "webpage": "https://web.njit.edu/~chasewu/",
+    "areas": [
+      "Machine learning",
+      "Computer networks",
+      "High-performance computing",
+      "Algorithms & complexity"
     ]
   },
   {
-    ""name"": ""Pan Xu"",
-    ""webpage"": ""https://sites.google.com/site/panxupi/home?authuser=0"",
-    ""areas"": [
-      ""Artificial intelligence"",
-      ""Machine learning"",
-      ""Algorithms & complexity"",
-      ""Economics & computation""
+    "name": "Pan Xu",
+    "webpage": "https://sites.google.com/site/panxupi/home?authuser=0",
+    "areas": [
+      "Artificial intelligence",
+      "Machine learning",
+      "Algorithms & complexity",
+      "Economics & computation"
     ]
   },
   {
-    ""name"": ""Mengjia Xu"",
-    ""webpage"": ""https://gracexu182.github.io/"",
-    ""areas"": [
-      ""Artificial intelligence"",
-      ""Computer vision"",
-      ""Machine learning"",
-      ""Natural language processing"",
-      ""Algorithms & complexity"",
-      ""Computational biology & bioinformatics""
+    "name": "Mengjia Xu",
+    "webpage": "https://gracexu182.github.io/",
+    "areas": [
+      "Artificial intelligence",
+      "Computer vision",
+      "Machine learning",
+      "Natural language processing",
+      "Algorithms & complexity",
+      "Computational biology & bioinformatics"
     ]
   },
   {
-    ""name"": ""Zephyr Yao"",
-    ""webpage"": ""https://web.njit.edu/~zy8/"",
-    ""areas"": [
-      ""Computer security"",
-      ""Mobile computing"",
-      ""Operating systems""
+    "name": "Zephyr Yao",
+    "webpage": "https://web.njit.edu/~zy8/",
+    "areas": [
+      "Computer security",
+      "Mobile computing",
+      "Operating systems"
     ]
   }
 ];
